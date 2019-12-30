b5bdf5a1e730b6e83eb74074ec3f6d78c8ac300c
Lucio D'Alessandro <lda@juxt.pro>
2019-12-30 11:29:03 +0000

    Improve readability and update tests

--- src/inst/core.cljc
+++ src/inst/core.cljc
@@ -1,27 +1,37 @@
 (ns inst.core
   (:require
-   [inst.utils :as inst]
+   [inst.utils :as utils]
    [tick.alpha.api :as t]))
 
 (defn time-since
   [[t t-now] & [config]]
   (let [vocabulary (merge-with #(or %2 %1)
-                               inst/default-vocabulary
+                               utils/default-vocabulary
                                (:vocabulary config))
-        intervals (inst/generate-intervals inst/default-intervals
+        intervals (utils/generate-intervals utils/default-intervals
                                            (:intervals config))
-        inst-now (when t-now (t/instant t-now))
-        seconds-from-event (inst/diff-in-seconds (t/instant t) (or inst-now (t/instant)))
-        interval (inst/find-interval intervals seconds-from-event)
-        time-value (inst/time-value seconds-from-event interval)
-        interval-name (inst/interval-name vocabulary interval time-value)
+        inst-now (when t-now
+                   (t/instant t-now))
+        seconds-from-event (utils/diff-in-seconds (t/instant t)
+                                                  (or inst-now (t/instant)))
+        interval (utils/find-interval intervals
+                                      seconds-from-event)
+        time-value (utils/time-value
+                    seconds-from-event
+                    interval)
+        interval-name (utils/interval-name
+                       vocabulary
+                       interval
+                       time-value)
         now? (= :now (first interval))
         order (if now?
                 [:interval]
                 (:order config [:time :interval :ago]))
-        ago (when-not now? {:ago (get vocabulary :ago)})]
-    (when (or (pos? seconds-from-event) (zero? seconds-from-event))
-      (inst/format-output order
+        ago (when-not now?
+              {:ago (get vocabulary :ago)})]
+    (when (or (pos? seconds-from-event)
+              (zero? seconds-from-event))
+      (utils/format-output order
                           (:stringify? config true)
                           (merge ago
                                  {:time time-value
@@ -30,23 +40,34 @@
 (defn time-to
   [[t t-now] & [config]]
   (let [vocabulary (merge-with #(or %2 %1)
-                               inst/default-vocabulary
+                               utils/default-vocabulary
                                (:vocabulary config))
-        intervals (inst/generate-intervals inst/default-intervals
+        intervals (utils/generate-intervals utils/default-intervals
                                            (:intervals config))
-        inst-now (when t-now (t/instant t-now))
-        seconds-from-event (inst/diff-in-seconds (t/instant t) (or inst-now (t/instant)))
-        abs-seconds (Math/abs seconds-from-event)
-        interval (inst/find-interval intervals abs-seconds)
-        time-value (inst/time-value abs-seconds interval)
-        interval-name (inst/interval-name vocabulary interval time-value)
+        inst-now (when t-now
+                   (t/instant t-now))
+        seconds-from-event (utils/diff-in-seconds
+                            (or inst-now (t/instant))
+                            (t/instant t))
+        interval (utils/find-interval
+                  intervals
+                  seconds-from-event)
+        time-value (utils/time-value
+                    seconds-from-event
+                    interval)
+        interval-name (utils/interval-name
+                       vocabulary
+                       interval
+                       time-value)
         now? (= :now (first interval))
         order (if now?
                 [:interval]
                 (:order config [:in :time :interval]))
-        in (when-not now? {:in (get vocabulary :in)})]
-    (when (or (neg? seconds-from-event) (zero? seconds-from-event))
-      (inst/format-output order
+        in (when-not now?
+             {:in (get vocabulary :in)})]
+    (when (or (pos? seconds-from-event)
+              (zero? seconds-from-event))
+      (utils/format-output order
                           (:stringify? config true)
                           (merge in
                                  {:time time-value
--- test/core.clj
+++ test/core.clj
@@ -1,44 +1,44 @@
 (ns test.core
   (:require
-   [inst.utils :as inst]
+   [inst.utils :as utils]
    [inst.core :as api]
    [tick.alpha.api :as t]
    [clojure.test :refer [deftest is testing run-tests]]))
 
 (deftest diff-in-seconds
   (testing "Event time and Now time difference in seconds"
-    (is (= 0 (inst/diff-in-seconds (t/instant "2019-12-31T11:00:00Z")
+    (is (= 0 (utils/diff-in-seconds (t/instant "2019-12-31T11:00:00Z")
                                    (t/instant "2019-12-31T11:00:00Z"))))
-    (is (= 20 (inst/diff-in-seconds (t/instant "2019-12-31T11:00:00Z")
+    (is (= 20 (utils/diff-in-seconds (t/instant "2019-12-31T11:00:00Z")
                                     (t/instant "2019-12-31T11:00:20Z"))))
-    (is (= 3000 (inst/diff-in-seconds (t/instant "2019-12-31T11:00:00Z")
+    (is (= 3000 (utils/diff-in-seconds (t/instant "2019-12-31T11:00:00Z")
                                       (t/instant "2019-12-31T11:50:00Z"))))
-    (is (= 25200 (inst/diff-in-seconds (t/instant "2019-12-31T11:00:00Z")
+    (is (= 25200 (utils/diff-in-seconds (t/instant "2019-12-31T11:00:00Z")
                                        (t/instant "2019-12-31T18:00:00Z"))))))
 
 (deftest time-value
   (testing "Final time number to be rendered. It's always a whole number"
-    (is (= 20 (inst/time-value 1200 [:minute {:seconds 60}])))
-    (is (= 1 (inst/time-value 3601 [:hour {:seconds 3600}])))
-    (is (= 2 (inst/time-value 259199 [:day {:seconds 86400}])))))
+    (is (= 20 (utils/time-value 1200 [:minute {:seconds 60}])))
+    (is (= 1 (utils/time-value 3601 [:hour {:seconds 3600}])))
+    (is (= 2 (utils/time-value 259199 [:day {:seconds 86400}])))))
 
 (deftest interval-name
   (testing "Final interval name to be rendered"
-    (is (= "just now" (inst/interval-name {:now "just now"}
+    (is (= "just now" (utils/interval-name {:now "just now"}
                                           [:now {:limit 6 :seconds 1}] 3)))
-    (is (= "minutes" (inst/interval-name {:minute ["minute" "minutes"]}
+    (is (= "minutes" (utils/interval-name {:minute ["minute" "minutes"]}
                                          [:minute {:limit 3600 :seconds 60}] 2)))
-    (is (= "minute" (inst/interval-name {:minute ["minute" "minutes"]}
+    (is (= "minute" (utils/interval-name {:minute ["minute" "minutes"]}
                                         [:minute {:limit 3600 :seconds 60}] 1)))
-    (is (= "days" (inst/interval-name {:day ["day" "days"]}
+    (is (= "days" (utils/interval-name {:day ["day" "days"]}
                                       [:day {:limit 3600 :seconds 60}] 2)))
-    (is (= "minuti" (inst/interval-name {:minute ["minuto" "minuti"]}
+    (is (= "minuti" (utils/interval-name {:minute ["minuto" "minuti"]}
                                         [:minute {:limit 3600 :seconds 60}] 2)))
-    (is (= "giorno" (inst/interval-name {:day ["giorno" "giorni"]}
+    (is (= "giorno" (utils/interval-name {:day ["giorno" "giorni"]}
                                         [:day {:limit 3600 :seconds 60}] 1)))
-    (is (= "hours" (inst/interval-name {:hour ["hour" "hours"]}
+    (is (= "hours" (utils/interval-name {:hour ["hour" "hours"]}
                                        [:hour {:limit 3600 :seconds 60}] 3)))
-    (is (= "days" (inst/interval-name {:day ["day" "days"]}
+    (is (= "days" (utils/interval-name {:day ["day" "days"]}
                                       [:day {:limit 3600 :seconds 60}] 3)))))
 
 (deftest find-interval
@@ -50,12 +50,10 @@
                      [:day {:limit 604800 :seconds 86400}]
                      [:week {:limit 2629743 :seconds 604800}]
                      [:month {:limit 31556926 :seconds 2629743}]
-                     [:year {:limit #?(:clj Long/MAX_VALUE
-                                       :cljs js/Number.MAX_SAFE_INTEGER)
-                             :seconds 31556926}]]]
-      (is (= [:hour {:limit 86400 :seconds 3600}] (inst/find-interval intervals 4000)))
-      (is (= [:second {:limit 60 :seconds 1}] (inst/find-interval intervals 59.9)))
-      (is (= [:month {:limit 31556926 :seconds 2629743}] (inst/find-interval intervals 4000000))))))
+                     [:year {:limit Long/MAX_VALUE :seconds 31556926}]]]
+      (is (= [:hour {:limit 86400 :seconds 3600}] (utils/find-interval intervals 4000)))
+      (is (= [:second {:limit 60 :seconds 1}] (utils/find-interval intervals 59.9)))
+      (is (= [:month {:limit 31556926 :seconds 2629743}] (utils/find-interval intervals 4000000))))))
 
 (deftest generate-intervals
   (testing "Sorted intervals"
@@ -66,21 +64,20 @@
             [:day {:limit 604800 :seconds 86400}]
             [:week {:limit 2629743 :seconds 604800}]
             [:month {:limit 31556926 :seconds 2629743}]
-            [:year {:limit #?(:clj Long/MAX_VALUE
-                              :cljs js/Number.MAX_SAFE_INTEGER)
+            [:year {:limit Long/MAX_VALUE
                     :seconds 31556926}]]
- (inst/generate-intervals default-intervals {})))
-    (let [intervals (inst/generate-intervals inst/default-intervals
+ (utils/generate-intervals default-intervals {})))
+    (let [intervals (utils/generate-intervals utils/default-intervals
                                              {:hour {:limit 90000}})]
       (is (= [:hour {:limit 90000 :seconds 3600}]
              (nth intervals 3))))
-    (let [intervals (inst/generate-intervals
-                     inst/default-intervals
+    (let [intervals (utils/generate-intervals
+                     utils/default-intervals
                      {:second {:limit 80 :seconds 2}})]
       (is (= [:second {:limit 80 :seconds 2}]
              (second intervals))))
-    (let [intervals (inst/generate-intervals
-                     inst/default-intervals
+    (let [intervals (utils/generate-intervals
+                     utils/default-intervals
                      {:now {:limit 30 :seconds 1}})]
       (is (= [:now {:limit 30 :seconds 1}]
              (first intervals))))))
@@ -88,31 +85,31 @@
 (deftest format-output
   (testing "Test different formattings"
     (is (= "12 seconds ago"
-           (inst/format-output [:time :interval :ago]
+           (utils/format-output [:time :interval :ago]
                                true
                                {:time 12
                                 :interval "seconds"
                                 :ago "ago"})))
     (is (= {:time 12 :interval "seconds" :ago "ago"}
-           (inst/format-output [:time :interval :ago]
+           (utils/format-output [:time :interval :ago]
                                false
                                {:time 12
                                 :interval "seconds"
                                 :ago "ago"})))
     (is (= "seconds 12 ago"
-           (inst/format-output [:interval :time :ago]
+           (utils/format-output [:interval :time :ago]
                                true
                                {:time 12
                                 :interval "seconds"
                                 :ago "ago"})))
     (is (= "in 12 seconds"
-           (inst/format-output [:in :time :interval]
+           (utils/format-output [:in :time :interval]
                                true
                                {:time 12
                                 :interval "seconds"
                                 :in "in"})))
     (is (= {:time 12 :interval "secondi" :in "fra"}
-           (inst/format-output [:in :time :interval]
+           (utils/format-output [:in :time :interval]
                                false
                                {:time 12
                                 :interval "secondi"
