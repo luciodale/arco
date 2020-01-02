7efbdc98e238cf69a07f1d35a6aafdf44a8d62b2
Lucio D'Alessandro <lda@juxt.pro>
2020-01-02 15:07:32 +0000

    Update name new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arco "0.2.2"
+(defproject arco "0.2.3"
   :description "Instant formatter to render time that has passed or is left since/to a certain event"
   :url "https://github.com/luciodale/arco"
   :license {:name "MIT"}
