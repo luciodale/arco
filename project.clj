72a7a3774b61c8f82459bde42f8d89fdf9a74cff
Lucio D'Alessandro <lda@juxt.pro>
2020-01-09 22:39:26 +0000

    Add tick dep to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject arco "0.2.3"
+(defproject arco "0.2.4"
   :description "Instant formatter to render time that has passed or is left since/to a certain event"
   :url "https://github.com/luciodale/arco"
   :license {:name "MIT"}
+  :dependencies [[tick "0.4.23-alpha"]]
   :source-paths ["src"]
   :profiles {:uberjar {:aot :all}}
   :deploy-repositories [["releases" :clojars]
